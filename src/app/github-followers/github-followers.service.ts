8a1b8291189b4e29600984802f98e10a4e08fea4
Khushboo <khagrawa@adobe.com>
2017-10-10 19:53:37 +0530

    Preparing for heroku

--- src/app/github-followers/github-followers.service.ts
+++ src/app/github-followers/github-followers.service.ts
@@ -1,6 +1,6 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/map';
+import "rxjs/add/operator/map";
 
 @Injectable()
 export class GithubFollowersService {
